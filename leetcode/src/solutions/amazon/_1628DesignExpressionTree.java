f8b6469209174f15a4f023a00c30abf5985a9b20
Joseph <liujingtao529@gmail.com>
2023-12-31 01:33:02 +1100

    1628 Design an Expression Tree With Evaluate Function

--- leetcode/src/solutions/amazon/_1628DesignExpressionTree.java
+++ leetcode/src/solutions/amazon/_1628DesignExpressionTree.java
@@ -1,20 +1,20 @@
 package solutions.amazon;
 
-import basic.TreeNode;
-import basic.TreeUtil;
+import java.util.Stack;
+import java.util.function.BiFunction;
 
 /**
  * NO.1628 Design an Expression Tree With Evaluate Function
- * Keywords:
+ * Keywords: stack, oop
  * Difficulty: Medium
  * Company: Amazon
  */
 public class _1628DesignExpressionTree {
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-        root.right = new TreeNode(7);
-        root.left = new TreeNode(8);
-        TreeUtil.show(root);
+        String[] postfix = {"3","4","+","2","*","7","/"};
+        TreeBuilder treeBuilder = new TreeBuilder();
+        Node node = treeBuilder.buildTree(postfix);
+        System.out.println(node.evaluate());
     }
 }
 
@@ -26,22 +26,37 @@ public class _1628DesignExpressionTree {
 abstract class Node {
     public abstract int evaluate();
     // define your fields here
-    protected String val;
-    protected Node left;
-    protected Node right;
-    public Node(String val){
-        this.val = val;
-    }
 };
-class MyNode extends Node{
-
-    public MyNode(String val) {
-        super(val);
+class NumberNode extends Node{
+    int val;
+    NumberNode(String val){
+        this.val = Integer.parseInt(val);
     }
-
     @Override
     public int evaluate() {
-        return 0;
+        return this.val;
+    }
+}
+class OperatorNode extends Node{
+    String operator;
+    Node left, right;
+    OperatorNode(String operator,Node left,Node right){
+        this.operator = operator;
+        this.left = left;
+        this.right = right;
+    }
+    private BiFunction<Integer, Integer, Integer> op(String operator){
+        switch (operator) {
+            case "+" : return (a, b) -> a + b;
+            case "-" : return (a, b) -> a - b;
+            case "*" : return (a, b) -> a * b;
+            case "/" : return (a, b) -> a / b;
+            default: throw new RuntimeException();
+        }
+    }
+    @Override
+    public int evaluate() {
+        return op(this.operator).apply(left.evaluate(), right.evaluate());
     }
 }
 
@@ -53,22 +68,20 @@ class MyNode extends Node{
  */
 
 class TreeBuilder {
-    //Node buildTree(String[] postfix) {
-    //
-    //}
-    ////postOrder = left subtree | right subtree | root
-    //MyNode dfs(String[] postOrder){
-    //    int index = 0;
-    //    for(int i = 0; i < postOrder.length; i++){
-    //        if(postOrder[i].equals("+") || postOrder[i].equals("-") || postOrder[i].equals("*") || postOrder[i].equals("/")){
-    //            index = i;
-    //            break;
-    //        }
-    //    }
-    //    MyNode root = new MyNode(postOrder[index]);
-    //    root.left = new MyNode(postOrder[index-2]);
-    //    root.right = new MyNode(postOrder[index-1]);
-    //}
+    Node buildTree(String[] postfix) {
+        Stack<Node> stack = new Stack<>();
+        for(String token : postfix){
+            if(Character.isDigit(token.charAt(0))){
+                stack.push(new NumberNode(token));
+            }else{
+                Node right = stack.pop();
+                Node left = stack.pop();
+                OperatorNode operatorNode = new OperatorNode(token,left,right);
+                stack.push(operatorNode);
+            }
+        }
+        return stack.pop();
+    }
 };
 
 
