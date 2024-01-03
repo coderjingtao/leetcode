557a8b64fd37175194dfa3780fe2dd73f95ade7a
Joseph <liujingtao529@gmail.com>
2024-01-03 21:42:46 +1100

    64 Minimum Path Sum

--- leetcode/src/solutions/amazon/_64MinimumPathSum.java
+++ leetcode/src/solutions/amazon/_64MinimumPathSum.java
@@ -1,5 +1,43 @@
-package solutions.amazon;/**
- * @author joseph
- * @create 2024-01-03
- */public class _64MinimumPathSum {
+package solutions.amazon;
+
+/**
+ * 64 Minimum Path Sum
+ * Keywords: dp, classic
+ * Difficulty: Medium
+ * Company: Amazon
+ */
+public class _64MinimumPathSum {
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        //first row
+        for(int i = 1; i < n ; i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        //first column
+        for(int i = 1; i < m ; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {1,3,1},
+                {1,5,1},
+                {4,2,1},
+        };
+        _64MinimumPathSum s = new _64MinimumPathSum();
+        int res = s.minPathSum(grid);
+        System.out.println("res = " + res);
+    }
+
 }
